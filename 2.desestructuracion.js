e5393ae36890774ca79d994590311bdd51c9433f
luwasd <lmanuel14@gmail.com>
2023-01-16 22:46:09 -0300

    desestructuracion 3

---
+++ 2.desestructuracion.js
@@ -0,0 +1,9 @@
+// Problema 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+

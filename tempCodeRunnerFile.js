b66beca4c1c06ab0a0855e62c265f3d3fc084c8d
luwasd <lmanuel14@gmail.com>
2023-02-02 00:25:49 -0300

    TiempoFinal2

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -41,33 +41,7 @@ while (primeCount < 1e6) {
     }
     num++;
 }
-console.log(`The 1,000,000th prime number is ${num - 1}`);
+console.log(`The 100,000th prime number is ${num - 1}`);
 console.log(`This took ${performance.now() - start3} milliseconds to run`);
 
 console.log("-----------------------------------------");
-
-// recursive
-function rFib(n) { // esta implementacion es mas lenta debido a que recursivamente llama a la funcion
-    if (n < 2) {
-        return n;
-    }
-    return rFib(n - 1) + rFib(n - 2);
-}
-console.log(rFib(20));
-// iterative
-function iFib(n) { // esta implementacion es mas rapida
-    const vals = [0, 1];
-    while (vals.length - 1 < n) {
-        let len = vals.length;
-        vals.push(vals[len - 1] + vals[len - 2]);
-    }
-    return vals[n];
-}
-console.log(iFib(20));
-
-console.log("-----------------------------------------");
-
-const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-const reversed1 = story.split("").reverse().join("");
-const reversedModificado = [...story].reverse().join(""); // metodo diferente con spread
-console.log(reversed1);

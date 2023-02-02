4cba42fdaaa7e2179e19c91795a3fd45f6a7505f
luwasd <lmanuel14@gmail.com>
2023-02-02 00:07:03 -0300

    TiempoFinal

--- 4.Cuanto tiempo tomo.js
+++ 4.Cuanto tiempo tomo.js
@@ -1,5 +1,5 @@
 Number.prototype.isPrime = function () {
-    for (let i = 2; i < this; i++) {
+    for (let i = 2; i <= Math.sqrt(this); i++) { // ahora la variable i solo llega a la raiz cuadrada y al no ser divisible es numero primo
         if (this % i === 0) {
             return false;
         }
@@ -11,7 +11,7 @@ const { performance } = require('perf_hooks');
 const start = performance.now();
 let primeCount = 0;
 let num = 2; // por razones matemáticas, 1 se considera primo
-while (primeCount < 10) {
+while (primeCount < 1e4) {
     if (num.isPrime()) {
         primeCount++;
     }
@@ -23,15 +23,15 @@ console.log(`This took ${performance.now() - start} milliseconds to run`);
 console.log("-----------------------------------------");
 
 // recursive
-function rFib(n) {
+function rFib(n) { // esta implementacion es mas lenta debido a que recursivamente llama a la funcion
     if (n < 2) {
         return n;
     }
     return rFib(n - 1) + rFib(n - 2);
 }
-rFib(20);
+console.log(rFib(20));
 // iterative
-function iFib(n) {
+function iFib(n) { // esta implementacion es mas rapida
     const vals = [0, 1];
     while (vals.length - 1 < n) {
         let len = vals.length;
@@ -39,11 +39,11 @@ function iFib(n) {
     }
     return vals[n];
 }
-iFib(20);
+console.log(iFib(20));
 
 console.log("-----------------------------------------");
 
 const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
 const reversed1 = story.split("").reverse().join("");
-const reversedModificado = [...story].reverse().join("");
+const reversedModificado = [...story].reverse().join(""); // metodo diferente con spread
 console.log(reversed1);
